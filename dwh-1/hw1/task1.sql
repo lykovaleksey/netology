b9c82419b86686900b82ed2a911c9f894c761df4
Lykov Aleksey <lykovaleksey@gmail.com>
2020-06-25 12:46:28 +0500

    update task1. update foreigin keys

--- dwh-1/hw1/task1.sql
+++ dwh-1/hw1/task1.sql
@@ -61,10 +61,11 @@ CREATE TABLE statuses (
     CONSTRAINT statuses_ukey UNIQUE(status_name)
 );
 
+
 CREATE TABLE years_film (
     year        integer,
-    film_id       varchar(10),
-      CONSTRAINT years_film_ukey UNIQUE(year,film_id)
+    film_id       integer,
+     CONSTRAINT years_film_ukey UNIQUE(year,film_id)
 );
 
 CREATE TABLE directors (
@@ -140,62 +141,21 @@ select distinct trim(s.token)
 FROM   films_raw t, unnest(string_to_array(t.studio, ',')) s(token)
 where   trim(s.token) != '';
 
-insert into public.studios_film (film_id , studio_id )
-select distinct id, st.studio_id from  films_raw t, unnest(string_to_array(t.studio, ',')) s(token)
-inner join studios st on st.studio_name  = trim(s.token)
-where   trim(s.token) != ''
-
---- Years
-WITH RECURSIVE r AS (
-	select t.id , t.dyear, min(s.token) minyear , max(cast( s.token as integer)) maxyear, min(cast(s.token as integer))  currentyear 
-	from (
-	select  distinct t.id , trim(s.token) dyear
-	from films_raw t,  unnest(string_to_array(t.year, ',')) s(token)
-	-- where id in (2160478, 2166535)
-	) t,  unnest(string_to_array(t.dyear, '-')) s(token)
-	group by t.id ,t.dyear 	
-	union 	
-	select  r.id as id,  r.dyear as dyear,   r. minyear  as minyear,  r.maxyear as maxyear, currentyear + 1 as currentyear
-	from r 
-	where currentyear < r.maxyear
-)
-insert into years_film (film_id , year)
-select id as film_id, currentyear as year from r
-order by r.id, r.currentyear;
-
 -- directors 
 
 insert into directors(director_name)
 select distinct trim(s.token) 
 from films_raw t,  unnest(string_to_array(t.director , ',')) s(token);
 
-
-insert into  public.directors_film (film_id ,director_id  )
-select distinct t.id, d.director_id 
-from films_raw t,  unnest(string_to_array(t.director , ',')) s(token)
-inner join directors d on d.director_name  =  trim(s.token) ;
-
 -- script_author 
 insert into authors(author_name)
 select distinct trim(s.token) 
 from films_raw t,  unnest(string_to_array(t.script_author , ',')) s(token);
 
-
-insert into  public.authors_film (film_id ,author_id  )
-select distinct t.id, d.author_id 
-from films_raw t,  unnest(string_to_array(t.script_author , ',')) s(token)
-inner join authors d on d.author_name  =  trim(s.token) ;
-
 -- cameramans 
 insert into cameramans(cameraman_name)
 select distinct trim(s.token) 
 from films_raw t,  unnest(string_to_array(t.cameraman , ',')) s(token);
-
-
-insert into  cameramans_film (film_id ,cameraman_id  )
-select distinct t.id, d.cameraman_id 
-from films_raw t,  unnest(string_to_array(t.cameraman , ',')) s(token)
-inner join cameramans d on d.cameraman_name  =  trim(s.token) ;
 
 ------------------ countries
 
@@ -203,10 +163,6 @@ insert into countries (country_name)
 select distinct trim( replace (s.token, E'\n','') )
 from films_raw t,  unnest(string_to_array(t.country , '|')) s(token)
 
-insert into countries_film (film_id ,country_id  )
-select distinct t.id, d.country_id 
-from films_raw t,  unnest(string_to_array(t.country , '|')) s(token)
-inner join countries d on d.country_name  =  trim( replace (s.token, E'\n','') ) ;
 -----------------------
 
 insert into statuses (status_name )
@@ -253,8 +209,93 @@ inner join ages a on a.age_name = fr.age
 inner join genres g on g.genre_name = fr.genre 
 inner join categories c on c.category_name = fr.category 
 
--- //TODO constraints
--- //TODO indexes
+
+-- m2m data
+
+insert into public.studios_film (film_id , studio_id )
+select distinct f.id, st.studio_id 
+from  films_raw t, films f, unnest(string_to_array(t.studio, ',')) s(token)
+inner join studios st on st.studio_name  = trim(s.token)
+where   trim(s.token) != ''  and f.film_key  = t.id;
+-- 
+WITH RECURSIVE r AS (
+	select t.id , t.dyear, min(s.token) minyear , max(cast( s.token as integer)) maxyear, min(cast(s.token as integer))  currentyear 
+	from (
+	select  distinct t.id , trim(s.token) dyear
+	from films_raw t,  unnest(string_to_array(t.year, ',')) s(token)
+	-- where id in (2160478, 2166535)
+	) t,  unnest(string_to_array(t.dyear, '-')) s(token)
+	group by t.id ,t.dyear 	
+	union 	
+	select  r.id as id,  r.dyear as dyear,   r. minyear  as minyear,  r.maxyear as maxyear, currentyear + 1 as currentyear
+	from r 
+	where currentyear < r.maxyear
+) 
+insert into years_film (film_id , year)
+select f.id as film_id, currentyear as year from  r
+inner join  films f on f.film_key  = r.id
+order by r.id, r.currentyear;
+
+
+insert into  cameramans_film (film_id ,cameraman_id  )
+select distinct f.id, d.cameraman_id 
+from films_raw t, films f,  unnest(string_to_array(t.cameraman , ',')) s(token)
+inner join cameramans d on d.cameraman_name  =  trim(s.token) 
+where   f.film_key  = t.id;
+
+insert into countries_film (film_id ,country_id  )
+select distinct f.id, d.country_id 
+from films_raw t, films f,  unnest(string_to_array(t.country , '|')) s(token)
+inner join countries d on d.country_name  =  trim( replace (s.token, E'\n','') ) 
+where   f.film_key  = t.id;
+
+insert into  public.authors_film (film_id ,author_id  )
+select distinct f.id, d.author_id 
+from films_raw t, films f,  unnest(string_to_array(t.script_author , ',')) s(token)
+inner join authors d on d.author_name  =  trim(s.token) 
+where   f.film_key  = t.id;
+
+insert into  public.directors_film (film_id ,director_id  )
+select distinct f.id, d.director_id 
+from films_raw t, films f,  unnest(string_to_array(t.director , ',')) s(token)
+inner join directors d on d.director_name  =  trim(s.token) 
+where   f.film_key  = t.id;
+
+
+-- constraints and indexes
+
+
+ALTER TABLE studios_film ADD CONSTRAINT fk_studios_film_fil FOREIGN KEY (film_id) REFERENCES films (id);
+ALTER TABLE studios_film ADD CONSTRAINT fk_studios_film_studio FOREIGN KEY (studio_id) REFERENCES studios (studio_id);
+CREATE  INDEX idx_uk_years_film ON years_film (year, film_id); 
+
+ALTER TABLE years_film ADD CONSTRAINT fk_years_film_film FOREIGN KEY (film_id) REFERENCES films (id);
+CREATE  INDEX idx_uk_films ON films(film_key);
+
+ALTER TABLE cameramans_film ADD CONSTRAINT fk_cameramans_film_film FOREIGN KEY (film_id) REFERENCES films (id);
+ALTER TABLE cameramans_film ADD CONSTRAINT fk_cameramans_film_cameraman_id FOREIGN KEY (cameraman_id) REFERENCES cameramans (cameraman_id);
+CREATE  INDEX idx_uk_cameramans_film ON cameramans_film(cameraman_id,film_id);
+
+
+ALTER TABLE countries_film ADD CONSTRAINT fk_countries_film_film FOREIGN KEY (film_id) REFERENCES films (id);
+ALTER TABLE countries_film ADD CONSTRAINT fk_countries_film_country_id FOREIGN KEY (country_id) REFERENCES countries (country_id);
+CREATE  INDEX idx_uk_countries_film ON countries_film(country_id,film_id);
+
+
+ALTER TABLE authors_film ADD CONSTRAINT fk_authors_filmfilm FOREIGN KEY (film_id) REFERENCES films (id);
+ALTER TABLE authors_film ADD CONSTRAINT fk_authors_film_author_id FOREIGN KEY (author_id) REFERENCES authors (author_id);
+CREATE  INDEX idx_uk_authors_film ON authors_film(author_id,film_id);
+
+ALTER TABLE directors_film ADD CONSTRAINT fk_directors_film FOREIGN KEY (film_id) REFERENCES films (id);
+ALTER TABLE directors_film ADD CONSTRAINT fk_directors_film_director_id FOREIGN KEY (director_id) REFERENCES directors (director_id);
+CREATE  INDEX idx_uk_director_film ON directors_film(director_id,film_id);
+
+
+
+
+
+
+
 
 
 
